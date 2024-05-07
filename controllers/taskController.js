e2f603d7e4a04595197dabc1425a0e57d1226929
Abhiram Kantipudi <a4abhiram125@gmail.com>
2024-05-07 16:25:22 +0530

    fix:Cannot set headers after they are sent to the client error

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -8,10 +8,9 @@ const createTask = async (req, res) => {
       return res.status(400).json({ error: "Title is required." });
     }
 
-    // To check wheather if there is user with assigned Id
-    let assignedUser = null;
+    // To check whether there is a user with the assigned ID
     if (assignee_id) {
-      assignedUser = await Users.findByPk(assignee_id);
+      let assignedUser = await Users.findByPk(assignee_id);
       if (!assignedUser) {
         return res
           .status(404)
@@ -19,6 +18,7 @@ const createTask = async (req, res) => {
       }
     }
 
+    // Create the task if everything is valid
     const newTask = await Tasks.create({
       title,
       description,
@@ -26,12 +26,12 @@ const createTask = async (req, res) => {
       assignee_id,
     });
 
-    res
+    return res
       .status(201)
       .json({ message: "Task created successfully", task: newTask });
   } catch (error) {
     console.error("Error creating task:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -40,22 +40,25 @@ const getAllTasks = async (req, res) => {
     let tasksList = await Tasks.findAll({
       include: [{ model: Users, as: "User", attributes: ["username"] }],
     });
-    res.status(200).json({ msg: "Tasks fetched succesfully", tasksList });
+    return res
+      .status(200)
+      .json({ msg: "Tasks fetched succesfully", tasksList });
   } catch (error) {
     console.error("Error Fetching tasks:", error);
-    res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 };
 
 const getTaskById = async (req, res) => {
   try {
     const { id } = req.params;
-    let task = await Tasks.findByPk(id);
+    let task = await Tasks.findByPk(id, {
+      include: [{ model: Users, as: "User", attributes: ["username"] }],
+    });
 
     if (!task) {
       return res.status(404).json({ error: `There is no task with id ${id}` });
     }
-
     return res
       .status(200)
       .json({ message: "Task fetched successfully", Task: task });
@@ -73,32 +76,28 @@ const deleteTaskById = async (req, res) => {
     if (!task) {
       return res.status(404).json({ error: `There is no task with id ${id}` });
     }
-
     await Tasks.destroy({
-      where: {
-        id: id,
-      },
+      where: { id: id },
     });
 
     return res
       .status(200)
-      .json({ msg: `task with id ${id} deleted succesfully` });
+      .json({ msg: `Task with id ${id} deleted successfully` });
   } catch (error) {
-    console.error("Error Deleting tasks:", error);
-    res.status(500).json({ error: error.message });
+    console.error("Error deleting task:", error);
+    return res.status(500).json({ error: error.message });
   }
 };
 
 const updateTaskById = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, status, assignee_id } = req.body;
   try {
-    const { id } = req.params;
-    const { title, description, status, assignee_id } = req.body;
     let task = await Tasks.findByPk(id);
 
     if (!task) {
       return res.status(404).json({ error: `There is no task with id ${id}` });
     }
-
     await task.update({
       title: title || task.title,
       description: description || task.description,
@@ -106,12 +105,12 @@ const updateTaskById = async (req, res) => {
       assignee_id: assignee_id || task.assignee_id,
     });
 
-    res
+    return res
       .status(200)
       .json({ msg: `Task with Id ${id} is updated successfully.` });
   } catch (error) {
     console.error("Error updating tasks:", error);
-    res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 };
 
