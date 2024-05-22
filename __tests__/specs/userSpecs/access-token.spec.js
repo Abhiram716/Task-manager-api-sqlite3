f9986cd86e6e8b399481564746ae815848be187c
Abhiram Kantipudi <a4abhiram125@gmail.com>
2024-05-22 16:34:16 +0530

    test:added test coverage for http 200 and 500 scenarios

--- __tests__/specs/userSpecs/access-token.spec.js
+++ __tests__/specs/userSpecs/access-token.spec.js
@@ -1,5 +1,15 @@
 import { faker } from '@faker-js/faker';
-import { beforeAll, beforeEach, describe, expect, it } from '@jest/globals';
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  jest,
+} from '@jest/globals';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import supertest from 'supertest';
 
 import app from '../../../app.js';
@@ -42,6 +52,56 @@ describe('Access Tokens', () => {
         });
         expect(response.status).toBe(401);
         expect(response.body.error).toEqual('Invalid password');
+      });
+    });
+    describe('Given Valid username and valid password', () => {
+      beforeAll(() => {
+        let accessToken;
+        let mockUser = {
+          dataValues: {
+            id: faker.string.uuid(),
+            username: faker.person.fullName(),
+            hashedPassword: bcrypt.hashSync('password', 10),
+          },
+        };
+        Users.findOne = jest.fn().mockResolvedValue(mockUser);
+        jwt.sign = jest.fn().mockImplementation(() => {
+          accessToken = 'mocked access token';
+          return accessToken;
+        });
+      });
+      afterAll(() => {
+        Users.findOne = jest.fn().mockReset();
+        jwt.sign = jest.fn().mockReset();
+      });
+      it('it should return status code 200 and access token', async () => {
+        const response = await superTestApp.post('/auth/acess-token').send({
+          username: faker.person.fullName(),
+          password: 'password',
+        });
+        console.log(response.error);
+        expect(response.status).toBe(200);
+        expect(response.body.accessToken).toBeDefined();
+      });
+    });
+    describe('if an error occurs during account creation', () => {
+      it('it should return 500 ', async () => {
+        const userPayload = {
+          username: faker.person.fullName(),
+          password: 'hashed_password',
+        };
+        const mockFindOne = jest.spyOn(Users, 'findOne');
+        mockFindOne.mockRejectedValue(new Error('Test error'));
+
+        const response = await superTestApp.post('/auth/acess-token').send({
+          username: userPayload.username,
+          password: userPayload.password,
+        });
+        await expect(Users.findOne(userPayload)).rejects.toThrow(Error);
+        expect(response.status).toBe(500);
+        expect(response.body.error).toEqual('Internal Server Error');
+
+        mockFindOne.mockRestore();
       });
     });
   });
